b9c1ed4edaf272c51b7bc433bfcc826768df4740
StasMarfenko <62810586+StasMarfenko@users.noreply.github.com>
2023-02-25 01:08:48 +0600

    feat: add greeting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // Add time and date
 let time = document.querySelector('.time');
 let date = document.querySelector('.date');
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+let months = ["января", "февраля", "марта", "мая", "апреля", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+let nowDate = new Date();
+
  const timer = () => {
     let now = new Date().toTimeString().replace(/ .*/, '');
     let sec = new Date().getSeconds();
@@ -8,11 +12,24 @@ let date = document.querySelector('.date');
   }
   setInterval(timer, 1000);
 
-  let nowDate = new Date();
-  let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-  let months = ["января", "февраля", "марта", "мая", "апреля", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
   date.innerHTML = week[nowDate.getDay()-1]+', '+nowDate.getDate()+' '+months[nowDate.getMonth()];
 
+// Add greeting
+let addName = document.querySelector('.name');
+let greeting = document.querySelector('.greeting');
+let arrGreeting = ['Доброй ночи', 'Доброе утро', 'Добрый день', 'Добрый вечер'];
+let min = nowDate.getMinutes();
+let hours = nowDate.getHours();
 
-
-
+function welcome(){
+    for(let i = 1; i < arrGreeting.length; i++){
+        return hours>=6*i&&hours<6*i+6 ? i : 0
+    }
+}
+greeting.innerHTML = arrGreeting[welcome()];
+addName.value = localStorage.getItem('yourName')
+addName.addEventListener('input', ()=>{
+    localStorage.clear();
+    localStorage.setItem('yourName', addName.value);
+})
+console.log();
